02e846bd09feabe53644dfbe745bdd2cfc88224b
Benjamin Patton <benjaminpatton@Benjamins-MacBook-Pro.local>
2024-01-27 10:24:44 -0500

    create github action

--- supabase/migrations/20240126203135_remote_schema.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."todos" add column "userId" uuid;
-
-
--- supabase/seed.sql
+++
@@ -1,5 +0,0 @@
-
-insert into todos(text) values ('pick up groceries'),
-('workout'),
-('take out trash'),
-('put gas in car')
